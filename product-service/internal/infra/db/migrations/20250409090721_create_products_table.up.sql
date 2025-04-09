ae19a94077bf5419a2dcecece34dea028c09c3bc
Oscar <chiu2000711@gmail.com>
2025-04-09 17:15:57 +0800

    feat: add products migration

---
+++ product-service/internal/infra/db/migrations/20250409090721_create_products_table.up.sql
@@ -0,0 +1,24 @@
+-- products table
+CREATE TABLE products (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price NUMERIC(10, 2) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+-- trigger function that updates the updated_at field whenever the row is updated
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.updated_at = CURRENT_TIMESTAMP;
+   RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+-- trigger on products table for each row update
+CREATE TRIGGER update_products_updated_at
+BEFORE UPDATE ON products
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
