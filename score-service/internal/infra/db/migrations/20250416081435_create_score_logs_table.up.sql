e0111784bd198c0501eb17b2cbf34086fb9a3185
Oscar <chiu2000711@gmail.com>
2025-04-16 16:26:11 +0800

    chore: add score_logs migration

---
+++ score-service/internal/infra/db/migrations/20250416081435_create_score_logs_table.up.sql
@@ -0,0 +1,24 @@
+CREATE TABLE score_logs (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    game UUID NOT NULL,
+    round INT NOT NULL,
+    winner_id BIGINT NOT NULL, -- 外鍵，指向 products.id
+    loser_id BIGINT NOT NULL,  -- 外鍵，指向 products.id
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+-- trigger function that updates the updated_at field whenever the row is updated
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.updated_at = CURRENT_TIMESTAMP;
+   RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+-- trigger on score_logs table for each row update
+CREATE TRIGGER update_score_logs_updated_at
+BEFORE UPDATE ON score_logs
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
